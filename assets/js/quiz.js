0c59847836f93eb48e8ff7d44159c64926416bb1
Andrew Ogilvie <primalorb@gmail.com>
2021-03-12 22:42:01 -0500

    quiz gameplay logic

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,11 +5,10 @@ var highScoreButton = document.getElementById( 'high-scores' ).addEventListener(
     highScoreHTML()
 })
 var timer = document.getElementById('timer');
-var questionEl; 
+var questionEl, questionID, timeInterval, timeLeft; 
 
 // On page load, populate DOM with start page
 startHTMl()
-var questionID = -1
 
 // Question Array
 var questions = [
@@ -79,6 +78,7 @@ function startHTMl() {
     startDiv.appendChild( startButton );
     container.appendChild( startDiv );
 
+        // listen to start button to begin the quiz
     document.getElementById( 'start-button' ).addEventListener( 'click', function() { 
         event.preventDefault();
         questionHTML();
@@ -119,8 +119,9 @@ function questionHTML() {
     responseDiv.appendChild( responseSpan );
     container.appendChild( responseDiv );
 
-    // start timer
+        // start timer
     timerStart()
+        // start quiz
     initQuiz()
 }
 
@@ -139,7 +140,7 @@ function endGameHTML() {
         //span  
     var scorePara = document.createElement( 'span' );
     scorePara.setAttribute( 'id', 'score-span' );
-    scorePara.innerHTML = `Your final score is <strong id="score">22</strong>`;
+    scorePara.innerHTML = `Your final score is <strong id="score">${timeLeft}</strong>`;
 
         //form
     var scoreForm = document.createElement( 'form' );
@@ -164,7 +165,8 @@ function endGameHTML() {
     scoreDiv.appendChild( scoreForm );
     container.appendChild( scoreDiv );
 
-    var submitButton = document.getElementById( 'submit' ).addEventListener( 'click', function() {
+        // listen to submit button to go to high score page
+    document.getElementById( 'submit' ).addEventListener( 'click', function() {
         event.preventDefault();
         highScoreHTML()
     })
@@ -207,20 +209,23 @@ function highScoreHTML() {
 
     container.appendChild( scoresDiv )
 
+        // listen to back button to return to start page
     document.getElementById( 'back-button' ).addEventListener( 'click', function() { 
         event.preventDefault();
         startHTMl();
     })
-
 }
 
 // Timer function
 function timerStart() {
-    var timeLeft = 25;
-    var timeInterval = setInterval(function() {
+    // set timer value
+    timeLeft = 25;
+    // timer interval countdown
+    timeInterval = setInterval(function() {
         timeLeft--
         timer.textContent = timeLeft + ' seconds'
         if( timeLeft === 0 ) {
+            // when timeout, stop tmie and go to end game
             clearInterval(timeInterval)
             endGameHTML()
         }
@@ -229,45 +234,41 @@ function timerStart() {
 
 // Init Quiz
 function initQuiz() {
+        // reset question counter
+    questionID = -1
+        // locate generated question element
     questionEl = document.getElementById('question')
-
         // shuffle question order
     shuffle(questions)
-
+        // run quiz loop
     iterateQuiz()
-
 }
 
 function iterateQuiz() {
-
-    
-    console.log(questions.length - 1, questionID)
+        // iterate question counter
     questionID = questionID + 1 
-
+        // if questions remain, run quiz on next item
     if ( questions.length - 1 >= questionID ) {
-    //shuffler answer order
-    shuffle(questions[questionID].answersObj)
-    
-    // display random question
-    questionEl.textContent = questions[questionID].question
-    var ol = document.getElementById('ol')
-    ol.innerHTML = ""
-
-    // loop in shuffled answer
-    for ( var i = 0; i < questions[questionID].answersObj.length; i++ ) {
-        var li = document.createElement('li')
-        li.textContent = questions[questionID].answersObj[i].answer
-        li.setAttribute( 'data-response', questions[questionID].answersObj[i].value )
-        ol.appendChild(li)
-    }
+            //shuffle answer order of current question
+        shuffle(questions[questionID].answersObj)
+            // generate HTML and display random question
+        questionEl.textContent = questions[questionID].question
+        var ol = document.getElementById('ol')
+        ol.innerHTML = ""
+            // loop in shuffled answer
+        for ( var i = 0; i < questions[questionID].answersObj.length; i++ ) {
+            var li = document.createElement('li')
+            li.textContent = questions[questionID].answersObj[i].answer
+            li.setAttribute( 'data-response', questions[questionID].answersObj[i].value )
+            ol.appendChild(li)
+        }
     } else {
+            // change to end game page
         endGameHTML()
-    }
-    
+            // stop timer
+        clearInterval(timeInterval)
+    } 
 }
-
-
-
 
 // Shuffler Arrays
 function shuffle(a) {
@@ -281,15 +282,15 @@ function shuffle(a) {
       // set the value of the same random index to the value of the original indexed selection
       a[random] = orig
     }
-  };
+};
 
   // Listen for true / false clicks on answers
-  container.addEventListener('click', function(e) {
+container.addEventListener('click', function(e) {
       var response = e.target.dataset.response
       console.log(response)
       if( response === 'true' || response === 'false' ) {
           console.log('answer')
           iterateQuiz()
       }
-  })
+})
 
